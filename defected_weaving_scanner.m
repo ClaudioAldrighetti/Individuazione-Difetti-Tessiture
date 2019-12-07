e71a8454062f2f23207fb337246415a58ce1d446
ClaudioAldrighetti <40755889+ClaudioAldrighetti@users.noreply.github.com>
2019-12-07 18:01:31 +0100

    Cross-correlazione normalizzata
    Aggiunta operazione di cross-correlazione normalizzata dei patterns sull'immagine.
    Correzione di alcuni errori nelle dimensioni dei pattern.
    Correzione inserimento e interazione dei pattern come celle della matrice patterns.
    Pulizia codice varia

--- defected_weaving_scanner.m
+++ defected_weaving_scanner.m
@@ -10,38 +10,41 @@ grayImg = rgb2gray(img);
 hR = round(row/2);
 hC = round(col/2);
 
+% Patterns
+pSize = 14;
+hP = round(pSize/2);
+patterns = {
+    grayImg(1:1+pSize, 1:1+pSize)       grayImg(1:1+pSize, hC-hP:hC+hP)     grayImg(1:1+pSize, col-pSize:col)
+    grayImg(hR-hP:hR+hP, 1:1+pSize)     grayImg(hR-hP:hR+hP, hC-hP:hC+hP)   grayImg(hR-hP:hR+hP, col-pSize:col)
+    grayImg(row-pSize:row, 1:1+pSize)   grayImg(row-pSize:row, hC-hP:hC+hP) grayImg(row-pSize:row, col-pSize:col)
+};
+
+% Image and patterns plot
 figure;
 subplot(1,2,1);
 imagesc(img); axis image; hold on;
 title('Image in RGB');
 subplot(1,2,2);
 imagesc(grayImg); axis image; colormap gray; hold on;
+rectangle('position',[1,1,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[hC-hP,1,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[col-pSize,1,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[1,hR-hP,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[hC-hP,hR-hP,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[col-pSize,hR-hP,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[1,row-pSize,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[hC-hP,row-pSize,pSize,pSize],'EdgeColor',[1 0 0]);
+rectangle('position',[col-pSize,row-pSize,pSize,pSize],'EdgeColor',[1 0 0]);
 title('Gray Image');
 
-% Patterns
-pSize = 14;
-hP = round(pSize/2);
-patterns = [
-    grayImg(1:pSize, 1:pSize)       grayImg(1:pSize, hC-hP:hC+hP)       grayImg(1:pSize, col-pSize:col)
-    grayImg(hR-hP:hR+hP, 1:pSize)   grayImg(hR-hP:hR+hP, hC-hP:hC+hP)   grayImg(hR-hP:hR+hP, col-pSize:col)
-    grayImg(row-pSize:row, 1:pSize) grayImg(row-pSize:row, hC-hP:hC+hP) grayImg(row-pSize:row, col-pSize:col)
-];
+% Test normalized cross-correlation between patterns and gray image
+pCrossCorr = cell(0,9);
+for i=1:9
+    pattern = cell2mat(patterns(i));
+    pCrossCorr(i) = {normxcorr2(pattern, grayImg)};
+end
 
-%%
-%rectangle('position',[1,1,14,14],'EdgeColor',[1 0 0]);
-%rectangle('position',[2,2,15,15],'EdgeColor',[1 0 0]);
-%rectangle('position',[R-13,C-13,14,14],'EdgeColor',[1 0 0]);
-%rectangle('position',[R-14,C-14,14,14],'EdgeColor',[1 0 0]);
-%rectangle('position',[1,C-13,14,14],'EdgeColor',[1 0 0]);
-%rectangle('position',[2,C-13,14,14],'EdgeColor',[1 0 0]);
-
-c1 = normxcorr2(pattern1,A);
-c2 = normxcorr2(pattern2,A);
-c3 = normxcorr2(pattern3,A);
-c4 = normxcorr2(pattern4,A);
-c5 = normxcorr2(pattern5,A);
-c6 = normxcorr2(pattern6,A);
-
+%% TODO
 c = (c1+c2+c3+c4+c5+c6)/6;
 c = c(12:end-12,12:end-12);
 figure, surf(abs(c)), shading flat
